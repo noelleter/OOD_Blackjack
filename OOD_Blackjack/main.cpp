e702903d12017732ae716f6d4484095174fba904
noelleter <shdmsrla@gmail.com>
2019-11-10 16:40:03 +0900

    Initial Commit

---
+++ OOD_Blackjack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOD_Blackjack
+//
+//  Created by 김노은 on 10/11/2019.
+//  Copyright © 2019 Noeun-Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
